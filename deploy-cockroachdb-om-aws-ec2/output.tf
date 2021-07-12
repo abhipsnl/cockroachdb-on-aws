0b7fc887a074c80d8fe3ebb8368918583988d883
Abhishek Sharma <abhishek.sharma@redacreltd.com>
2021-07-13 00:07:34 +0530

    pushed day2 changes

--- deploy-cockroachdb-om-aws-ec2/output.tf
+++ deploy-cockroachdb-om-aws-ec2/output.tf
@@ -1,3 +1,43 @@
 output "vpc-id" {
   value = module.create_cockroachdb_environment.vpc-id
 }
+
+output "id" {
+  description = "List of IDs of instances"
+  value       = module.create_cockroachdb_environment.id
+}
+
+output "availability_zone" {
+  description = "List of availability zones of instances"
+  value       = module.create_cockroachdb_environment.availability_zone
+}
+
+output "key_name" {
+  description = "List of key names of instances"
+  value       = module.create_cockroachdb_environment.key_name
+}
+
+output "public_ip" {
+  description = "List of public IP addresses assigned to the instances, if applicable"
+  value       = module.create_cockroachdb_environment.public_ip
+}
+
+output "private_ip" {
+  description = "List of private IP addresses assigned to the instances"
+  value       = module.create_cockroachdb_environment.private_ip
+}
+
+output "security_groups" {
+  description = "List of associated security groups of instances"
+  value       = module.create_cockroachdb_environment.security_groups
+}
+
+output "instance_state" {
+  description = "List of instance states of instances"
+  value       = module.create_cockroachdb_environment..instance_state
+}
+
+output "instance_count" {
+  description = "Number of instances to launch specified as argument to this module"
+  value       = var.instance_count
+}
--- terraform-modules/computing/output.tf
+++ terraform-modules/computing/output.tf
@@ -1,3 +1,43 @@
 output "vpc-id" {
   value = aws_vpc.test-vpc.id
 }
+
+output "id" {
+  description = "List of IDs of instances"
+  value       = aws_instance.cockroachdb-node.*.id
+}
+
+output "availability_zone" {
+  description = "List of availability zones of instances"
+  value       = aws_instance.cockroachdb-node.*.availability_zone
+}
+
+output "key_name" {
+  description = "List of key names of instances"
+  value       = aws_instance.cockroachdb-node.*.key_name
+}
+
+output "public_ip" {
+  description = "List of public IP addresses assigned to the instances, if applicable"
+  value       = aws_instance.cockroachdb-node.*.public_ip
+}
+
+output "private_ip" {
+  description = "List of private IP addresses assigned to the instances"
+  value       = aws_instance.cockroachdb-node.*.private_ip
+}
+
+output "security_groups" {
+  description = "List of associated security groups of instances"
+  value       = aws_instance.cockroachdb-node.*.security_groups
+}
+
+output "instance_state" {
+  description = "List of instance states of instances"
+  value       = aws_instance.cockroachdb-node.*.instance_state
+}
+
+output "instance_count" {
+  description = "Number of instances to launch specified as argument to this module"
+  value       = var.instance_count
+}
