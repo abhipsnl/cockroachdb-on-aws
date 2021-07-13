ee3fe62a482b984aea080655e7b7418935617248
Abhishek Sharma <abhishek.sharma@redacreltd.com>
2021-07-13 19:20:03 +0530

    Added more generic ways

--- deploy-cockroachdb-om-aws-ec2/variables.tf
+++ deploy-cockroachdb-om-aws-ec2/variables.tf
@@ -38,8 +38,8 @@ variable "ami" {
     type = map
 
     default = {
-        default    = "ami-0c2a1acae6667e438"
-        ap-south-1 = "ami-059d1007dcf297b22"
+        default    = "ami-008a8487adc2b32ec"
+        ap-south-1 = "ami-008a8487adc2b32ec"
         eu-west-2  = "ami-03dea29b0216a1e03"
         us-east-1  = "ami-0c2a1acae6667e438"
     }
--- scripts/setup-cockroach.sh
+++ scripts/setup-cockroach.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -ex
-curl https://binaries.cockroachdb.com/cockroach-v21.1.5.darwin-10.9-amd64.tgz | tar -xJ && cp -i cockroach-v21.1.5.darwin-10.9-amd64/cockroach /usr/local/bin/
-mkdir -p  /var/lib/cockroach
+wget -qO- https://binaries.cockroachdb.com/cockroach-v21.1.5.darwin-10.9-amd64.tgz | tar  xvz
+cp cockroach-v20.1.6.linux-amd64/cockroach /usr/local/bin/ -f
+mkdir /var/lib/cockroach
 useradd cockroach
 chown -R cockroach.cockroach /var/lib/cockroach
 
