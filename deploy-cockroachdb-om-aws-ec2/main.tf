988c45e8d0562df15c78e5c23c2ff9cac8b30105
Abhishek Sharma <abhishek.sharma@redacreltd.com>
2021-07-13 17:25:39 +0530

    Added more generic ways

--- deploy-cockroachdb-om-aws-ec2/main.tf
+++ deploy-cockroachdb-om-aws-ec2/main.tf
@@ -1,11 +1,7 @@
-//locals {
-//  environment = lookup(var.workspace_to_environment_map, terraform.workspace, "dev")
-//  size = local.environment == "dev" ? lookup(var.workspace_to_size_map, terraform.workspace, "small") : var.environment_to_size_map[local.environment]
-//  region = local.environment == "dev" ? lookup(var.environment_to_region_map, terraform.workspace, "ap-south-1") : var.environment_to_region_map[local.environment]
-//  ami = var.ami[local.region]
-//  count = lookup(var.instance_count, terraform.workspace, "2")
-//  instance_type = var.instance_type[local.environment]
-//}
+locals {
+  region = var.environment_to_region_map[terraform.workspace]
+  ami = var.ami[local.region]
+}
 
 module "create_cockroachdb_environment" {
   source = "git::https://github.com/sharmajee1/cockroachdb-on-aws.git//terraform-modules/computing"
@@ -14,7 +10,7 @@ module "create_cockroachdb_environment" {
   public_key_path = var.public_key_path
   region = var.environment_to_region_map[terraform.workspace]
   instance_count = var.instance_count[terraform.workspace]
-  ami = var.ami[var.region]
+  ami = var.ami[local.region]
   instance_type = var.instance_type[terraform.workspace]
   ec2_user = var.ec2_user
   vpc_cidr_map = var.vpc_cidr_map[terraform.workspace]
