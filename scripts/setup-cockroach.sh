cdbd2989b00639ea7a1f56aa728a677c02a4d6a3
Abhishek Sharma <abhishek.sharma@redacreltd.com>
2021-07-13 18:13:29 +0530

    Added more generic ways

--- scripts/setup-cockroach.sh
+++ scripts/setup-cockroach.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -ex
-wget -qO- https://binaries.cockroachdb.com/cockroach-v20.1.6.linux-amd64.tgz | tar  xvz
-cp cockroach-v20.1.6.linux-amd64/cockroach /usr/local/bin/ -f
-mkdir /var/lib/cockroach
+curl https://binaries.cockroachdb.com/cockroach-v21.1.5.darwin-10.9-amd64.tgz | tar -xJ && cp -i cockroach-v21.1.5.darwin-10.9-amd64/cockroach /usr/local/bin/
+mkdir -p  /var/lib/cockroach
 useradd cockroach
 chown -R cockroach.cockroach /var/lib/cockroach
 
