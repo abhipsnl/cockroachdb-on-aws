768f4fb0160c675d36ad2a678961bb078889d91d
Abhishek Sharma <abhishek.sharma@redacreltd.com>
2021-07-13 23:45:42 +0530

    modified shell script

--- scripts/setup-cockroach.sh
+++ scripts/setup-cockroach.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 set -ex
-ls -lrt 
 
-wget -qO- https://binaries.cockroachdb.com/cockroach-v21.1.5.darwin-10.9-amd64.tgz | tar  xvz
+VERSION="v21.1.5"
 
-cp cockroach-v20.1.6.linux-amd64/cockroach /usr/local/bin/ -f
+function install_cockroachdb_cli() {
 
-mkdir /var/lib/cockroach
-useradd cockroach
-chown -R cockroach.cockroach /var/lib/cockroach
+    if [ -f /usr/local/bin/cockroach ];then
+        echo "Already installed, skipping"
+        exit 0
+    fi
 
-echo "Successfully fetched and installed"
+    wget -qO- https://binaries.cockroachdb.com/cockroach-${VERSION}.darwin-10.9-amd64.tgz | tar  xvz
+    if [ $? --ne 0 ];then
+        echo "Failed to download cockroachdb CLI"
+        exit 1
+    fi
+
+    cp cockroach-${VERSION}.linux-amd64/cockroach /usr/local/bin/ -f
+
+    mkdir /var/lib/cockroach
+    useradd cockroach
+    chown -R cockroach.cockroach /var/lib/cockroach
+
+    echo "Successfully fetched and installed"
+
+}
+
+install_cockroachdb_cli
